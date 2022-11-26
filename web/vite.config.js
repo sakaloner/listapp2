90d292307e1ae979ac045bcaac86d353b81375b1
sakaloner <andresf40y0@gmail.com>
2022-11-25 22:06:52 -0500

    cualquier cosa

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,17 +2,18 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import mkcert from 'vite-plugin-mkcert';
-//import fs from "node:fs";
+//import mkcert from 'vite-plugin-mkcert';
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), mkcert()],
-  server: { https: true},
-  // https: {
-  //   key: fs.readFileSync("/home/ubuntu/coding/listapp2/web/certs/key.pem"),
-  //   cert: fs.readFileSync("/home/ubuntu/coding/listapp2/web/certs/cert1.pem"),
-  // },
+  server: { 
+    https: {
+      key: fs.readFileSync('./.cert/key.pem'),
+      cert: fs.readFileSync('./.cert/cert.pem'),
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
