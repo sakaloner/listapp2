229da80a65f5a0d7ea875b086ecbc14bef2d26c2
sakaloner <andresf40y0@gmail.com>
2022-11-26 03:10:57 +0000

    created a vite config file

---
+++ web/vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+//import mkcert from 'vite-plugin-mkcert';
+import fs from "fs";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), mkcert()],
+  server: { 
+    https: {
+      key: fs.readFileSync('./.cert/key.pem'),
+      cert: fs.readFileSync('./.cert/cert.pem'),
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
