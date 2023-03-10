ee371c7c4d03738f108e36224e5a885228cd6145
sakaloner <andresf40y0@gmail.com>
2023-03-10 11:24:49 -0500

    port things

--- webr/next.config.js
+++ webr/next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     MAIN_URL: process.env.MAIN_URL
-  },
-  async rewrites () {
-    return [
-      {
-        source: 'http://localhost:80/:path*', // Match all paths
-        destination: 'http://localhost:3000/:path*' // Pass through to the next server
-      }
-    ]
   }
 }
 
